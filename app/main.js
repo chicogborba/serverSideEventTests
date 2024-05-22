2ae339b2babd289c5dccee0cdda26f64b953b547
chicogborba <chicogborba@gmail.com>
2024-05-22 19:57:11 -0300

    adding codes suport to generate diferent sse

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 
 console.log('Hello from main.js');
 
-let sse = new EventSource('http://localhost:8080/stream');
+let sse = new EventSource('http://localhost:8080/stream/666');
 
 sse.onmessage = (event) => {
   console.log(event.data);
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -6,23 +7,46 @@ app.use((req, res, next) => {
   next();
 });
 
+// Middleware para analisar o corpo da solicitação JSON
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get("/stream", (req,res) => {
-  res.setHeader("Content-Type", "text/event-stream");
+// Array de códigos
+let codes = ['1A', '1B', '1C', '1D'];
 
-  let count = 1;
-  let intervalId = setInterval(() => {
-    res.write("data: " + count + "\n\n");
-    count++;
-    if(count > 100) {
-      clearInterval(intervalId);
-      res.end();
-    }
-  }, 500);
-})
+// Iterar sobre os códigos para criar um endpoint para cada um
+codes.forEach(code => {
+  createSSEEndpoint(code);
+});
+
+// Função para criar um endpoint SSE
+function createSSEEndpoint(code) {
+  app.get(`/stream/${code}`, (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream");
+
+    let count = 1;
+    let intervalId = setInterval(() => {
+      // Aqui você pode enviar informações diferentes para cada código
+      res.write(`data: ${code} - ${count}\n\n`);
+      count++;
+      if(count > 100) {
+        clearInterval(intervalId);
+        res.end();
+      }
+    }, 500);
+  });
+}
+
+// Endpoint para adicionar um novo código
+app.post('/addCode', (req, res) => {
+  const newCode = req.body.code;
+  codes.push(newCode);
+  createSSEEndpoint(newCode);
+  res.send(`Code ${newCode} added successfully.`);
+});
 
 app.listen(8080)
 console.log('Server running at http://localhost:8080/');
